41de7450099788730a175bc0506c5f6709133178
Andrew Ray <andrew.ray@grandrounds.com>
2021-06-29 09:45:22 -0700

    [CARESERV-928] Renaming Entity in gqlgen to avoid type name conflict
    [CARESERV-928](https://grandrounds.atlassian.net/browse/CARESERV-928)

--- example/federation/products/graph/generated/federation.go
+++ example/federation/products/graph/generated/federation.go
@@ -3,17 +3,21 @@
 package generated
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"strings"
+"context"
+"errors"
+"fmt"
+"strings"
+"github.com/99designs/gqlgen/plugin/federation/fedruntime")
 
-	"github.com/99designs/gqlgen/plugin/federation/fedruntime"
-)
 
-func (ec *executionContext) __resolve__service(ctx context.Context) (fedruntime.Service, error) {
+
+
+
+
+
+func (ec *executionContext) __resolve__service(ctx context.Context) (fedruntime.GqlgenService, error) {
 	if ec.DisableIntrospection {
-		return fedruntime.Service{}, errors.New("federated introspection disabled")
+		return fedruntime.GqlgenService{}, errors.New("federated introspection disabled")
 	}
 
 	var sdl []string
@@ -25,37 +29,43 @@ func (ec *executionContext) __resolve__service(ctx context.Context) (fedruntime.
 		sdl = append(sdl, src.Input)
 	}
 
-	return fedruntime.Service{
+	return fedruntime.GqlgenService{
 		SDL: strings.Join(sdl, "\n"),
 	}, nil
 }
 
-func (ec *executionContext) __resolve_entities(ctx context.Context, representations []map[string]interface{}) ([]fedruntime.Entity, error) {
-	list := []fedruntime.Entity{}
+
+func (ec *executionContext) __resolve_entities(ctx context.Context, representations []map[string]interface{}) ([]fedruntime.fedruntime.GqlgenEntity, error) {
+	list := []fedruntime.fedruntime.GqlgenEntity{}
 	for _, rep := range representations {
 		typeName, ok := rep["__typename"].(string)
 		if !ok {
 			return nil, errors.New("__typename must be an existing string")
 		}
 		switch typeName {
-
-		case "Product":
-			id0, err := ec.unmarshalNString2string(ctx, rep["upc"])
-			if err != nil {
-				return nil, errors.New(fmt.Sprintf("Field %s undefined in schema.", "upc"))
-			}
-
-			entity, err := ec.resolvers.Entity().FindProductByUpc(ctx,
-				id0)
-			if err != nil {
-				return nil, err
-			}
-
-			list = append(list, entity)
-
+		
+			
+			case "Product":
+				id0, err := ec.unmarshalNString2string(ctx, rep["upc"])
+					if err != nil {
+						return nil, errors.New(fmt.Sprintf("Field %s undefined in schema.", "upc"))
+					}
+				
+	
+				entity, err := ec.resolvers.Entity().FindProductByUpc(ctx,
+					id0, )
+				if err != nil {
+					return nil, err
+				}
+	
+				
+				list = append(list, entity)
+			
+		
 		default:
-			return nil, errors.New("unknown type: " + typeName)
+			return nil, errors.New("unknown type: "+typeName)
 		}
 	}
 	return list, nil
 }
+
